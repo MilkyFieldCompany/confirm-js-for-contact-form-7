226d017b347e5ceebc9452f9e25b6e8e6d4b4b2e
MilkyField <milkyfieldcompany@gmail.com>
2021-08-10 14:55:53 +0900

    use wp_enqueue commands
    use wp_enqueue commands.
    code clena up.

--- cjsfcf7.php
+++ cjsfcf7.php
@@ -14,14 +14,10 @@ This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
 published by the Free Software Foundation.
 
-*/	
+*/
 class cjsfcf7 {
-	var $table_name_option;
-	var $table_name_cash;
-	
-	function __construct() {
 
-		global $wpdb;
+	function __construct() {
 
 		load_plugin_textdomain("confirm-js-for-contact-form-7", false, basename(dirname(__FILE__)).'/languages');
 
@@ -33,27 +29,23 @@ class cjsfcf7 {
 	}
 
 	function fnAddScript() {
-
-		$confirmjspath = plugins_url("cjsfcf7.js", __FILE__);
-
 ?>
-			<script type="text/javascript">
-				var CJSFCF7 = {
-					CLASS_WPCF7_FORM			: 'wpcf7',
-					CLASS_WPCF7_ALERT_MESSAGE	: 'wpcf7-not-valid-tip',
-					BUTTON_TEXT_CONFIRM			: '<?php _e('Confirm','confirm-js-for-contact-form-7'); ?>',
-					BUTTON_TEXT_TO_CORRECT		: '<?php _e('To correct','confirm-js-for-contact-form-7'); ?>', 
-					ERRMSG_EMPTY				: '<?php _e('Please enter the required items.','confirm-js-for-contact-form-7'); ?>',
-					ERRMSG_INVALIDE_EMAIL_ADDR	: '<?php _e('The email address format doesn`t seem to be correct.','confirm-js-for-contact-form-7'); ?>',
-					ERRMSG_EMAIL_ADDR_UNMATCH	: '<?php _e('The confirmation email addresses do not match.','confirm-js-for-contact-form-7'); ?>',
-					ERRMSG_INVALIDE_URL			: '<?php _e('There is a mistake in the URL.','confirm-js-for-contact-form-7'); ?>',
-					ERRMSG_INVALIDE_TEL			: '<?php _e('The phone number is incorrect.','confirm-js-for-contact-form-7'); ?>',
-				};
-			</script>
-			<script src="<?php echo esc_url($confirmjspath); ?>"> </script>
+		<script type="text/javascript">
+			var CJSFCF7 = {
+				CLASS_WPCF7_FORM			: 'wpcf7',
+				CLASS_WPCF7_ALERT_MESSAGE	: 'wpcf7-not-valid-tip',
+				BUTTON_TEXT_CONFIRM			: '<?php _e('Confirm','confirm-js-for-contact-form-7'); ?>',
+				BUTTON_TEXT_TO_CORRECT		: '<?php _e('To correct','confirm-js-for-contact-form-7'); ?>',
+				ERRMSG_EMPTY				: '<?php _e('Please enter the required items.','confirm-js-for-contact-form-7'); ?>',
+				ERRMSG_INVALIDE_EMAIL_ADDR	: '<?php _e('The email address format doesn`t seem to be correct.','confirm-js-for-contact-form-7'); ?>',
+				ERRMSG_EMAIL_ADDR_UNMATCH	: '<?php _e('The confirmation email addresses do not match.','confirm-js-for-contact-form-7'); ?>',
+				ERRMSG_INVALIDE_URL			: '<?php _e('There is a mistake in the URL.','confirm-js-for-contact-form-7'); ?>',
+				ERRMSG_INVALIDE_TEL			: '<?php _e('The phone number is incorrect.','confirm-js-for-contact-form-7'); ?>',
+			};
+		</script>
 <?php
+		wp_enqueue_script( 'custom-script', plugins_url("cjsfcf7.js", __FILE__), array( 'jquery' ) );
 	}
-
 }
 new cjsfcf7();
 ?>
